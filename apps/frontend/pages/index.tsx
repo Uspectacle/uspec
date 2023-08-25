ca21d53b0f18813c5012a7e2e2772f6601a066e0
Uspectacle <mario@galadrim.fr>
2023-08-25 18:12:32 +0200

    test: remove everything except setup v2

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 import React from 'react';
 
 const Blog: NextPage = () => {
-  return <>page in maintenance</>;
+  return <>Blog in maintenance, please come back in an hour</>;
 };
 
 export default Blog;
